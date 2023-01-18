89459a0df20d570800faaf2c4a0a4198c2eac924
iesparag <iesparagjain@gmail.com>
2023-01-18 20:32:18 +0530

    branch parag

--- tech_world/src/App.js
+++ tech_world/src/App.js
@@ -4,14 +4,19 @@ import SimpleSidebar from "./Admin/Pages/Admin";
 import Dashboard from "./Admin/Components/Dashboard";
 import AddForm from "./Admin/Components/Form";
 import Admin from "./Admin/Pages/Admin.jsx";
+import SingleCard from "./Admin/Components/SingleCard";
+import AllProduct from "./Admin/Components/AllProduct";
 function App() {
   return (
     <div>
       {/* <h1>Hello World</h1> */}
       <Routes>
         <Route path="/admin" element={<Admin />} />
-        {/* <Route path="/admin/dashboard" element={<Dashboard />} /> */}
+        <Route path="/admin/dashboard" element={<Dashboard />} />
+        <Route path="/admin/allproduct" element={<AllProduct />} />
         {/* <Route path="/admin/addproduct" element={<AddForm />} /> */}
+        <Route path="/card" element={<SingleCard />} />
+        {/* <Route path="/allproduct" element={<AllProduct />} /> */}
         {/* <Route path="/" element={< />} /> */}
       </Routes>
     </div>
