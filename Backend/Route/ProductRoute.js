77d2091654a0e0149559d084a7e851ce4dbf1e64
raj-1313 <theromeo.1313@gmail.com>
2023-01-19 00:45:15 +0530

    sorting done

--- Backend/Route/ProductRoute.js
+++ Backend/Route/ProductRoute.js
@@ -12,18 +12,15 @@ app.get("/", async (req, res) => {
       let products = await productModel.find({
         model: { $regex: find, $options: "i" },
       }).limit(limit).skip(limit*(page-1));
-
       res.send(products);
 
-
     } else {
-
       let products = await productModel.find().limit(limit).skip((page-1)*limit);
 console.log(products.length)
       res.send(products)
-
     }
-  } catch (e) {
+  } 
+  catch (e) {
     res.send(e.message);
   }
 });
