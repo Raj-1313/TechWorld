2a38237b46939ec743560af7070967411cd57203
iesparag <iesparagjain@gmail.com>
2023-01-19 00:45:15 +0530

    parag code day_2

--- Backend/Route/ProductRoute.js
+++ Backend/Route/ProductRoute.js
@@ -5,13 +5,7 @@ const VisitingMiddleware = require("../middleware/RouteChack_Middleware");
 
 
 
-app.get("/",  async (req,res)=>{
-    try{
-        const data= await productModel.find()
-        res.send(data)
-    }
-    catch(e){
-        res.send(e.message)
+
 
 app.get("/", async (req, res) => {
   const find = req.query.find;
@@ -27,12 +21,10 @@ app.get("/", async (req, res) => {
 
     } else {
 
-      let products = await productModel.find().limit(limit).skip((page-1)*limit);
+      let  products = await productModel.find().limit(limit).skip((page-1)*limit);
 console.log(products.length)
       res.send(products)
 
-      let products = await productModel.find();
-      res.send(products);
 
 
     }
--- tech_world/src/Admin/Components/AllProduct.jsx
+++ tech_world/src/Admin/Components/AllProduct.jsx
@@ -1,34 +1,56 @@
-import { Box, Grid } from "@chakra-ui/react";
+import { Box, Grid, Button, Center } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
+import { adminData } from "../../Redux/AdminRedux/Admin_Action";
 import SingleCard from "./SingleCard";
+import { useDispatch, useSelector } from "react-redux";
 
 const AllProduct = () => {
-  const [data, setData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totaldocuments, setTotaldocuments] = useState(null);
+  const dispatch = useDispatch();
+
+  const { AdminData, totalPages } = useSelector((state) => state.Admin_reducer);
+  console.log(currentPage);
   useEffect(() => {
-    Alldata();
-  }, []);
+    dispatch(adminData({ page: currentPage, limit: 10 }));
+  }, [dispatch, currentPage]);
 
-  const Alldata = () => {
-    fetch("http://localhost:8080/product")
-      .then((res) => res.json())
-      .then((res) => setData(res));
-  };
+  // const Alldata = (page = 1, limit = 10) => {
+  // .then((res) => res.json())
+  // .then((res) => {
+  //   setData(res.products);
+  //   setCurrentPage(res.currentPage);
+  //   setTotaldocuments(res.count);
+  //   setTotalPage(res.totalPages);
+  //   console.log(res);
+  // });
+  // };
 
-  console.log(data);
+  // console.log(currentPage, totalPages, totaldocuments);
 
   return (
     <Box>
       <Grid
+        gap={5}
         gridTemplateColumns={{
           base: "1fr",
           md: "1fr ",
           lg: "1fr 1fr 1fr",
         }}
       >
-        {data?.map((product) => (
+        {AdminData?.map((product) => (
           <SingleCard {...product} key={product._id} />
         ))}
       </Grid>
+      <Center>
+        <Button>Prev</Button>
+
+        <Button>
+          {currentPage} of {totalPages}
+        </Button>
+
+        <Button onClick={() => setCurrentPage((pr) => pr + 1)}>Next</Button>
+      </Center>
     </Box>
   );
 };
