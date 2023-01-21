6621b6f8a1d209b361a4a4afefc7290823edebbd
subhamukherjee321 <subhamukherjee321@gmail.com>
2023-01-21 16:44:02 +0530

    Form Data Onchange Update

--- tech_world/src/Components/Payment/Form1.jsx
+++ tech_world/src/Components/Payment/Form1.jsx
@@ -1,61 +1,75 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
-    Progress,
-    Box,
-    ButtonGroup,
-    Button,
-    Heading,
-    Flex,
-    FormControl,
-    GridItem,
-    FormLabel,
-    Input,
-    Select,
-    SimpleGrid,
-    InputLeftAddon,
-    InputGroup,
-    Textarea,
-    FormHelperText,
-    InputRightElement,
-} from '@chakra-ui/react';
+  Heading,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  FormHelperText,
+} from "@chakra-ui/react";
 
-const form1Data = {
-    FName: "",
-    LName: "",
-    email: "",
-}
+const initState = {
+  FName: "",
+  LName: "",
+  Email: "",
+};
 
-const Form1 = () => {
-    const [data, setData] = useState(form1Data);
-    return (
-        <>
-            <Heading w="100%" textAlign={'center'} fontWeight="normal" mb="2%">
-                Payment Details
-            </Heading>
-            <Flex>
-                <FormControl mr="5%">
-                    <FormLabel htmlFor="first-name" fontWeight={'normal'}>
-                        First name
-                    </FormLabel>
-                    <Input id="first-name" placeholder="First name" />
-                </FormControl>
+const Form1 = ({getFrom1Data}) => {
+  const [data, setData] = useState(initState);
 
-                <FormControl>
-                    <FormLabel htmlFor="last-name" fontWeight={'normal'}>
-                        Last name
-                    </FormLabel>
-                    <Input id="last-name" placeholder="First name" />
-                </FormControl>
-            </Flex>
-            <FormControl mt="2%">
-                <FormLabel htmlFor="email" fontWeight={'normal'}>
-                    Email address
-                </FormLabel>
-                <Input id="email" type="email" />
-                <FormHelperText>We'll never share your email.</FormHelperText>
-            </FormControl>
-        </>
-    );
+  const handleChange = (e) => {
+    let { name, value } = e.target;
+    setData({ ...data, [name]: value });
+    getFrom1Data(data);
+  };
+
+  return (
+    <>
+      <Heading w="100%" textAlign={"center"} fontWeight="normal" mb="2%">
+        Payment Details
+      </Heading>
+      <Flex>
+        <FormControl mr="5%">
+          <FormLabel htmlFor="first-name" fontWeight={"normal"}>
+            First name
+          </FormLabel>
+          <Input
+            id="first-name"
+            placeholder="First name"
+            onChange={handleChange}
+            name={"FName"}
+            value={data.FName}
+          />
+        </FormControl>
+
+        <FormControl>
+          <FormLabel htmlFor="last-name" fontWeight={"normal"}>
+            Last name
+          </FormLabel>
+          <Input
+            id="last-name"
+            placeholder="First name"
+            onChange={handleChange}
+            name={"LName"}
+            value={data.LName}
+          />
+        </FormControl>
+      </Flex>
+      <FormControl mt="2%">
+        <FormLabel htmlFor="email" fontWeight={"normal"}>
+          Email address
+        </FormLabel>
+        <Input
+          id="email"
+          type="email"
+          onChange={handleChange}
+          name={"Email"}
+          value={data.Email}
+        />
+        <FormHelperText>We'll never share your email.</FormHelperText>
+      </FormControl>
+    </>
+  );
 };
 
 export default Form1;
