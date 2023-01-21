bf96cc44d51092eec554c95f1f3c0ffced8c534e
RAJ RATHOR <theromeo.1313@gmail.com>
2023-01-21 15:00:57 +0530

    resolver store

--- tech_world/src/Redux/Store.js
+++ tech_world/src/Redux/Store.js
@@ -9,15 +9,25 @@ import { Admin_reducer } from "./AdminRedux/Admin_Reducer";
 import LoginReducer from "./LoginRedux/Login.Reducer";
 import searhReducer from "./SearchRedux/Search.Reducer";
 import signupReducer from "./SignupRedux/Signup.Reducer";
+import {reducer as AppReducer} from "./AppReducer/reducer";
+import {reducer as CartReducer} from "./CartRedux/reducer"
+import {reducer as ReduceItemReducer} from "./ReduceItemCart/reducer"
+import {reducer as AddItemReducer} from "./AddItemCart/reducer"
+import {reducer as DeleteItemReducer} from "./DeleteItemCart/reducer"
 
 const rootReducer = combineReducers({
-  Admin_reducer,
-  search: searhReducer,
   signup: signupReducer,
   login: LoginReducer,
+  AppReducer,
+  Admin_reducer,
+  search: searhReducer,
+  CartReducer,
+  ReduceItemReducer,
+  AddItemReducer,
+  DeleteItemReducer
 });
 
-const composerEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composerEnhancer = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
 
 const store = legacy_createStore(
   rootReducer,
